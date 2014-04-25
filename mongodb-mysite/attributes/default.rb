c3db8e47611b9097d110fea91f604a02f668eca7
edweip3 <edwei.push@gmail.com>
2014-04-25 15:40:32 -0400

    more chef tests

--- mongodb-mysite/attributes/default.rb
+++ mongodb-mysite/attributes/default.rb
@@ -1,2 +1,2 @@
-node.mongodb[:config][:dbpath] = '/mnt/disk1/mongodb'
-node.mongodb[:config][:logpath] = '/mnt/disk1/mongodb/mongodb.log'
+node.default.mongodb[:config][:dbpath] = '/mnt/disk1/mongodb'
+node.default.mongodb[:config][:logpath] = '/mnt/disk1/mongodb/mongodb.log'
