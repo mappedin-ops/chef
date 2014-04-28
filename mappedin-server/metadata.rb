447a3ec813f0188a0c1eef1664549cb52f78a35d
edweip3 <edwei.push@gmail.com>
2014-04-28 10:45:04 -0400

    mappedin-server cookbook
    currently only sets the NODE_PATH env variable

---
+++ mappedin-server/metadata.rb
@@ -0,0 +1,6 @@
+name  "mappedin-server"
+description "cookbook for deploying mappedin-server"
+maintainer  "Ed"
+license  "Apache 2.0"
+
+depends 'magic_shell'
---
+++ mappedin-server/recipes/default.rb
@@ -0,0 +1,4 @@
+
+magic_shell_environment 'NODE_PATH' do
+  value '$NODE_PATH:./library'
+end
