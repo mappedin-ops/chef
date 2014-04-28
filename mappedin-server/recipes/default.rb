e80415f021a45ce32cbae49e06b4cf730f8974de
edweip3 <edwei.push@gmail.com>
2014-04-28 15:17:55 -0400

    more fixes

--- mappedin-server/recipes/default.rb
+++ mappedin-server/recipes/default.rb
@@ -1,4 +1,7 @@
+node[:deploy].each do |application, deploy|
+    deploy = node[:deploy][application]
 
-magic_shell_environment 'NODE_PATH' do
-  value node[:deploy]['mappedinserver'][:deploy_to] + '/current'
+    magic_shell_environment 'NODE_PATH' do
+      value deploy[:deploy_to] + '/current:$NODE_PATH'
+    end
 end
