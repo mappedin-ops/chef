dc6af82c37cbaa8289d89fdb851994398e974493
edweip3 <edwei.push@gmail.com>
2014-04-28 15:36:15 -0400

    more bugs
    forgot to add 'library' to the path.

--- mappedin-server/recipes/default.rb
+++ mappedin-server/recipes/default.rb
@@ -2,6 +2,6 @@ node[:deploy].each do |application, deploy|
     deploy = node[:deploy][application]
 
     magic_shell_environment 'NODE_PATH' do
-      value deploy[:deploy_to] + '/current:$NODE_PATH'
+      value deploy[:deploy_to] + '/current/library:$NODE_PATH'
     end
 end
