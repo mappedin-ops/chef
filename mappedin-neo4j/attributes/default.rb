511e209abfc16930611be8d44d372b2a3d5fd812
edweip3 <edwei.push@gmail.com>
2014-04-29 16:47:50 -0400

    correcting lib dir

--- mappedin-neo4j/attributes/default.rb
+++ mappedin-neo4j/attributes/default.rb
@@ -2,4 +2,4 @@ node.default['java']['jdk_version'] = 7
 node.default['java']['accept_license_agreement'] = true
 
 node.default[:neo4j][:server][:data_dir] = '/mnt/disk1/neo4j/data/graph.db'
-node.default[:neo4j][:server][:lib_dir] = default[:neo4j][:server][:installation_dir] + '/lib'
+node.default[:neo4j][:server][:lib_dir] = '/mnt/disk1/neo4j'
